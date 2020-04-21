1f597394ec121d2e2899d659adac8a4ecfe2ec62
Adam Oswalt <email@adamoswalt.com>
2020-04-21 16:16:37 -0500

    Add search query using union

--- ch04/lib/plate_slate_web/schema.ex
+++ ch04/lib/plate_slate_web/schema.ex
@@ -32,5 +32,10 @@ defmodule PlateSlateWeb.Schema do
       arg :order, :sort_order, default_value: :asc
       resolve &Resolvers.Menu.menu_items/3
     end
+
+    field :search, list_of(:search_result) do
+      arg :matching, non_null(:string)
+      resolve &Resolvers.Menu.search/3
+    end
   end
 end
