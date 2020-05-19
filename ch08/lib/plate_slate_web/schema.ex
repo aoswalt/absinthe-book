72cbbd107ed97e496e75fa683dedab96f3b07400
Adam Oswalt <email@adamoswalt.com>
2020-05-19 13:11:25 -0500

    Add authentication mutation

--- ch08/lib/plate_slate_web/schema.ex
+++ ch08/lib/plate_slate_web/schema.ex
@@ -17,6 +17,7 @@ defmodule PlateSlateWeb.Schema do
     middleware
   end
 
+  import_types __MODULE__.AccountsTypes
   import_types __MODULE__.MenuTypes
   import_types __MODULE__.OrderingTypes
 
@@ -72,6 +73,13 @@ defmodule PlateSlateWeb.Schema do
   end
 
   mutation do
+    field :login, :session do
+      arg :email, non_null(:string)
+      arg :password, non_null(:string)
+      arg :role, non_null(:role)
+      resolve &Resolvers.Accounts.login/3
+    end
+
     field :place_order, :order_result do
       arg :input, non_null(:place_order_input)
       resolve &Resolvers.Ordering.place_order/3
