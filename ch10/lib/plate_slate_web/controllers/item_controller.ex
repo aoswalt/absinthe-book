04974a186a6ba0ddfbc9e50329898badb350a2a1
Adam Oswalt <email@adamoswalt.com>
2020-06-02 19:21:06 -0500

    Use graphql query for items index

--- ch10/lib/plate_slate_web/controllers/item_controller.ex
+++ ch10/lib/plate_slate_web/controllers/item_controller.ex
@@ -1,60 +1,15 @@
 defmodule PlateSlateWeb.ItemController do
   use PlateSlateWeb, :controller
+  use Absinthe.Phoenix.Controller, schema: PlateSlateWeb.Schema
 
-  alias PlateSlate.Menu
-  alias PlateSlate.Menu.Item
-
-  def index(conn, _params) do
-    items = Menu.list_items()
-    render(conn, "index.html", items: items)
-  end
-
-  def new(conn, _params) do
-    changeset = Menu.change_item(%Item{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
-  def create(conn, %{"item" => item_params}) do
-    case Menu.create_item(item_params) do
-      {:ok, item} ->
-        conn
-        |> put_flash(:info, "Item created successfully.")
-        |> redirect(to: item_path(conn, :show, item))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    item = Menu.get_item!(id)
-    render(conn, "show.html", item: item)
-  end
-
-  def edit(conn, %{"id" => id}) do
-    item = Menu.get_item!(id)
-    changeset = Menu.change_item(item)
-    render(conn, "edit.html", item: item, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "item" => item_params}) do
-    item = Menu.get_item!(id)
-
-    case Menu.update_item(item, item_params) do
-      {:ok, item} ->
-        conn
-        |> put_flash(:info, "Item updated successfully.")
-        |> redirect(to: item_path(conn, :show, item))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", item: item, changeset: changeset)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    item = Menu.get_item!(id)
-    {:ok, _item} = Menu.delete_item(item)
-
-    conn
-    |> put_flash(:info, "Item deleted successfully.")
-    |> redirect(to: item_path(conn, :index))
+  @graphql """
+  {
+    menu_items {
+      name
+    }
+  }
+  """
+  def index(conn, result) do
+    render(conn, "index.html", items: result.data["menu_items"] || [])
   end
 end
